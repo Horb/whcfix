369e80f75f0b13573d726d802427500a82d713de
Adam Morgan <horb.adam@gmail.com>
2014-01-16 13:54:56 +0000

    Initial support for stripping fixures from FixturesLive

--- data.py
+++ data.py
@@ -10,6 +10,55 @@ def getConfig():
     with open('config.json') as jsonFile:
         return json.loads(jsonFile.read())
 
+
+def getMatchesFixturesLive(teamNumber, teamName):
+    url = "http://w.fixtureslive.com/team/%s/fixtures/%s" % (teamNumber, teamName)
+    print url
+    r = requests.get(url)
+    return r.content
+
+def tryParseMatchFixturesLive(htmlContent):
+    soup = BeautifulSoup(htmlContent)
+    teamName = "Wakefield 1s"
+    for tr in soup("tr"):
+        if len(tr) != 9:
+            continue
+        children = [child for child in tr.childGenerator()]
+#            0 
+#            1 Brooklands MU Men's 1s
+#            2 
+#            3 0:3
+#            4 NPMHL MCN
+#            5 14.09.13 16:30
+#            6 A
+#            7 BHC
+#            8 Fixture ID: 1336350HistoryAdd to calendarAudit trailHow post-match details are addded
+        _, oposition, _, score, league, date_time, home_away, venue, _ = children
+
+        if ":" in score.text:
+            homeGoals, awayGoals = score.text.split(":")
+            homeGoals, awayGoals = map(int, (homeGoals, awayGoals))
+        elif score.text == "&nbsp;":
+            homeGoals, awayGoals = None, None
+
+        date, time = date_time.text.split(" ")
+        venue = venue.text
+        if home_away.text == "A":
+            home = oposition.text
+            away = teamName
+        else:
+            home = teamName
+            away = oposition.text
+
+        yield {'date':date,
+                'time':time,
+                'venue':venue,
+                'home':home,
+                'homeGoals':homeGoals,
+                'awayGoals':awayGoals,
+                'away':away}
+            
+
 def getMatchesHTML(leagueId, clubId=0):
     url = "http://www.yorkshireha.org.uk/e107_plugins/league_manager/index.php?fixtures"
     payload = {
@@ -80,3 +129,8 @@ def getMatchDicts(leagueId, clubId=0):
         if matchDict is not None:
             listOfMatches.append(matchDict)
     return listOfMatches
+
+if __name__ == '__main__':
+    content = getMatchesFixturesLive("1031", "Wakefield-Mens-1s")
+    for match in tryParseMatchFixturesLive(content):
+        print match
