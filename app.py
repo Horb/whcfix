1272b11f3144ee22a08e7f28a6dc8c4ca3fb322a
Adam Morgan <horb.adam@gmail.com>
2014-06-12 13:16:12 +0100

    PEP8 enforcement.

--- app.py
+++ app.py
@@ -6,58 +6,62 @@ if __name__ == '__main__':
     current_directory = os.path.dirname(os.path.realpath(__file__))
     os.chdir(current_directory)
 
-app = Flask(__name__
-        , template_folder='whcfix/templates'
-        , static_folder='whcfix/static'
-        , static_url_path='/static')
+app = Flask(__name__,
+            template_folder='whcfix/templates',
+            static_folder='whcfix/static',
+            static_url_path='/static')
+
 
 @app.route("/")
 def maintenance():
-    return render_template("maintenance.html", strings = appStrings())
+    return render_template("maintenance.html",
+                           strings=appStrings())
 
-#@app.route("/")
-#def hello():
-#    matches = models.Matches()
-#    teams = matches.teamNames("Wakefield")
-#    return render_template("dashboard.html"
+# @app.route("/")
+# def hello():
+#     matches = models.Matches()
+#     teams = matches.teamNames("Wakefield")
+#     return render_template("dashboard.html"
 #                           , strings = logic.appStrings()
 #                           , recent_form = matches.recentForm(teams)
 #                           , nextMatches = matches.getNextMatches(teams)
 #                           , lastResults = matches.getLastResults(teams)
-#                           )
-#@app.route("/recent_form/")
-#def recent_form():
-#    matches = models.Matches()
-#    teams = matches.teamNames("Wakefield")
-#    return render_template("recent_form.html"
+#                            )
+# @app.route("/recent_form/")
+# def recent_form():
+#     matches = models.Matches()
+#     teams = matches.teamNames("Wakefield")
+#     return render_template("recent_form.html"
 #                           , recent_form = matches.recentForm(teams)
-#                           )
-#
-#@app.route("/last_result/")
-#def last_result():
-#    matches = models.Matches()
-#    teams = matches.teamNames("Wakefield")
-#    return render_template("last_result.html"
+#                            )
+
+# @app.route("/last_result/")
+# def last_result():
+#     matches = models.Matches()
+#     teams = matches.teamNames("Wakefield")
+#     return render_template("last_result.html"
 #                           , lastResults = matches.getLastResults(teams)
-#                           )
-#
-#@app.route("/next_match/")
-#def next_match():
-#    matches = models.Matches()
-#    teams = matches.teamNames("Wakefield")
-#    return render_template("next_match.html"
+#                            )
+
+# @app.route("/next_match/")
+# def next_match():
+#     matches = models.Matches()
+#     teams = matches.teamNames("Wakefield")
+#     return render_template("next_match.html"
 #                           , nextMatches = matches.getNextMatches(teams)
-#                           )
-#
-#@app.route("/teams/<team>/")
-#def team(team):
-#    matches = models.Matches()
-#    return render_template("teamDump.html", team = team, matches = matches.teamFilter(team))
-#
-#@app.route("/teams/")
-#def teams():
-#    matches = models.Matches()
-#    return render_template("teamDump.html", team = "All", matches = matches.listOfMatches)
+#                            )
+
+# @app.route("/teams/<team>/")
+# def team(team):
+#     matches = models.Matches()
+#     return render_template("teamDump.html",
+#                            team = team, matches = matches.teamFilter(team))
+
+# @app.route("/teams/")
+# def teams():
+#     matches = models.Matches()
+#     return render_template("teamDump.html",
+#                            team = "All", matches = matches.listOfMatches)
 
 if __name__ == '__main__':
     app.debug = True
