61487b4f96a4ee607414ce7e095956fca6fa751d
Adam Morgan <horb.adam@gmail.com>
2016-08-31 01:05:16 +0100

    Refactor data access to be functional.

--- whcfix/data/yorkshirehockeyassociationadapter.py
+++ whcfix/data/yorkshirehockeyassociationadapter.py
@@ -1,148 +1,167 @@
-import requests
-import logging
-from BeautifulSoup import BeautifulSoup
 import datetime
-from whcfix.data.adapterbase import AdapterBase
+import logging
+import requests
+from BeautifulSoup import BeautifulSoup
+from whcfix.logic.match import Match
+
+nbsp = '&nbsp;'
 
 
-class YorkshireHockeyAssociationAdapter(AdapterBase):
+def _getMatchObjectFromDict(matchDict, sectionName):
+    date = matchDict['date']
+    time = matchDict['time']
+    venue = matchDict['venue']
+    home = matchDict['home']
+    homeGoals = matchDict['homeGoals']
+    awayGoals = matchDict['awayGoals']
+    isPostponed = matchDict['isPostponed']
+    away = matchDict['away']
+    return Match(date, time, venue, home, homeGoals, awayGoals, away,
+                 isPostponed, sectionName)
 
-    def __init__(self, leagueId, clubId, sectionName):
-        super(YorkshireHockeyAssociationAdapter, self).__init__(sectionName)
-        self.leagueId = leagueId
-        self.clubId = clubId
+# yorkshire_mens = get_matches("Mens", 204, home_away=1, club_id=66)
+# yorkshire_ladies = get_matches("Ladies", 205, home_away=1, club_id=66)
+# for m in yorkshire_mens + yorkshire_ladies:
+#     m.date, m.home, m.away
 
-    def get_matches(self):
-        dicts = self._get_match_dicts_from_HTML(self._get_HTML())
-        return [self._getMatchObjectFromDict(d) for d in dicts]
+def get_matches(sectionName, league_id, club_id="", date="all", division="", home_away="0"):
+    html = _get_HTML(league_id, club_id, date, division, home_away)
+    dicts = _get_match_dicts_from_HTML(html)
+    return [_getMatchObjectFromDict(d, sectionName) for d in dicts]
 
-    def _get_HTML(self):
-        url = "http://www.yorkshireha.org.uk/e107_plugins/league_manager/index.php?fixtures"
-        payload = {"leagman_club": self.clubId, "leagman_date": "",
-                   "leagman_division": "", "leagman_fixtures_search_form": "1",
-                   "leagman_fixtures_search_form": "1", "leagman_game": "0",
-                   "leagman_home_away": "0,",
-                   "leagman_league_id": self.leagueId, "leagman_team": "",
-                   "leagman_venue": "0"}
-        r = requests.post(url,
-                          headers={'user-agent': 'whcfix.com/1.0'},
-                          data=payload)
-        return r.content
+def _get_HTML(league_id, club_id, date, division, home_away):
+    url = "http://www.yorkshireha.org.uk/e107_plugins/league_manager/index.php?fixtures"
+    payload = {
+            "leagman_club": club_id, 
+            "leagman_date": date,
+            "leagman_division": division, 
+            "leagman_fixtures_search_form": "1",
+            "leagman_fixtures_search_form": "1", 
+            "leagman_game": "0",
+            "leagman_home_away": home_away,
+            "leagman_league_id": league_id, 
+            "leagman_team": "",
+            "leagman_venue": "0"
+            }
+    r = requests.post(url,
+                      headers={'user-agent': 'whcfix.com/1.0'},
+                      data=payload)
+    return r.content
 
-    def _get_match_dicts_from_HTML(self, html):
-        soup = BeautifulSoup(html)
-        listOfMatches = []
-        for tr in soup("tr"):
-            matchDict = self._parse_row(tr)
-            if matchDict is not None:
-                listOfMatches.append(matchDict)
-        return listOfMatches
+def _get_match_dicts_from_HTML(html):
+    soup = BeautifulSoup(html)
+    listOfMatches = []
+    for tr in soup("tr"):
+        matchDict = _parse_row(tr)
+        if matchDict is not None:
+            listOfMatches.append(matchDict)
+    return listOfMatches
 
-    def _parse_row(self, tr):
-        try:
-            tds = tr("td")
-            if len(tds) != 6:
-                return None
-            date_td, time_td, venue_td, home_td, result_td, away_td = tds
-            date = self._parse_date(date_td)
-            time = self._parse_time(time_td)
-            venue = self._parse_venue(venue_td)
-            home = self._parse_home(home_td)
-            homeGoals = self._parse_homeGoals(result_td)
-            awayGoals = self._parse_awayGoals(result_td)
-            isPostponed = self._parse_isPostponed(result_td)
-            away = self._parse_away(away_td)
-
-            return_dict = {'date': date,
-                           'time': time,
-                           'venue': venue,
-                           'home': home,
-                           'homeGoals': homeGoals,
-                           'awayGoals': awayGoals,
-                           'isPostponed': isPostponed,
-                           'away': away}
-            return return_dict
-        except Exception:
-            logging.exception("")
+def _parse_row(tr):
+    try:
+        tds = tr("td")
+        if len(tds) != 6:
             return None
+        date_td, time_td, venue_td, home_td, result_td, away_td = tds
+        date = _parse_date(date_td)
+        time = _parse_time(time_td)
+        venue = _parse_venue(venue_td)
+        home = _parse_home(home_td)
+        homeGoals = _parse_homeGoals(result_td)
+        awayGoals = _parse_awayGoals(result_td)
+        isPostponed = _parse_isPostponed(result_td)
+        away = _parse_away(away_td)
 
-    def _parse_date(self, date_td):
-        try:
-            return datetime.datetime.strptime(date_td.text, '%d %b %y')
-        except Exception:
-            logging.exception(date_td.text)
+        return_dict = {'date': date,
+                       'time': time,
+                       'venue': venue,
+                       'home': home,
+                       'homeGoals': homeGoals,
+                       'awayGoals': awayGoals,
+                       'isPostponed': isPostponed,
+                       'away': away}
+        return return_dict
+    except Exception:
+        logging.exception("")
+        return None
+
+def _parse_date(date_td):
+    try:
+        return datetime.datetime.strptime(date_td.text, '%d %b %y')
+    except Exception:
+        logging.exception(date_td.text)
+        return None
+
+def _parse_time(time_td):
+    try:
+        if time_td.text == '--:--':
             return None
+        return datetime.datetime.strptime(time_td.text, '%H:%M')
+    except Exception:
+        logging.exception(time_td.text)
+        return None
 
-    def _parse_time(self, time_td):
-        try:
-            if time_td.text == '--:--':
-                return None
-            return datetime.datetime.strptime(time_td.text, '%H:%M')
-        except Exception:
-            logging.exception(time_td.text)
+def _parse_venue(venue_td):
+    try:
+        s = venue_td.text
+        return " ".join(s.split(nbsp))
+    except Exception:
+        logging.exception(venue_td.text)
+        return None
+
+def _parse_home(home_td):
+    try:
+        s = home_td.text
+        return " ".join(s.split(nbsp))
+    except Exception:
+        logging.exception(home_td.text)
+        return None
+
+def _parse_away(away_td):
+    try:
+        s = away_td.text
+        return " ".join(s.split(nbsp))
+    except Exception:
+        logging.exception(away_td.text)
+        return None
+
+def _parse_homeGoals(result_td):
+    try:
+        s = result_td.text
+        if s == nbsp + nbsp:
             return None
-
-    def _parse_venue(self, venue_td):
-        try:
-            s = venue_td.text
-            return " ".join(s.split(self.nbsp))
-        except Exception:
-            logging.exception(venue_td.text)
-            return None
-
-    def _parse_home(self, home_td):
-        try:
-            s = home_td.text
-            return " ".join(s.split(self.nbsp))
-        except Exception:
-            logging.exception(home_td.text)
-            return None
-
-    def _parse_away(self, away_td):
-        try:
-            s = away_td.text
-            return " ".join(s.split(self.nbsp))
-        except Exception:
-            logging.exception(away_td.text)
-            return None
-
-    def _parse_homeGoals(self, result_td):
-        try:
-            s = result_td.text
-            if s == self.nbsp + self.nbsp:
+        else:
+            scores = s.split(nbsp)
+            if 'P' in scores:
                 return None
             else:
-                scores = s.split(self.nbsp)
-                if 'P' in scores:
-                    return None
-                else:
-                    return int(scores[0])
-        except Exception:
-            logging.exception(result_td.text)
-            return None
+                return int(scores[0])
+    except Exception:
+        logging.exception(result_td.text)
+        return None
 
-    def _parse_awayGoals(self, result_td):
-        try:
-            s = result_td.text
-            if s == self.nbsp + self.nbsp:
+def _parse_awayGoals(result_td):
+    try:
+        s = result_td.text
+        if s == nbsp + nbsp:
+            return None
+        else:
+            scores = s.split(nbsp)
+            if 'P' in scores:
                 return None
             else:
-                scores = s.split(self.nbsp)
-                if 'P' in scores:
-                    return None
-                else:
-                    return int(scores[-1])
-        except Exception:
-            logging.exception(result_td.text)
-            return None
+                return int(scores[-1])
+    except Exception:
+        logging.exception(result_td.text)
+        return None
 
-    def _parse_isPostponed(self, result_td):
-        try:
-            s = result_td.text
-            if 'P' in s:
-                return True
-            else:
-                return False
-        except Exception:
-            logging.exception(result_td.text)
+def _parse_isPostponed(result_td):
+    try:
+        s = result_td.text
+        if 'P' in s:
+            return True
+        else:
             return False
+    except Exception:
+        logging.exception(result_td.text)
+        return False
--- whcfix/logic/matchesbase.py
+++ whcfix/logic/matchesbase.py
@@ -1,14 +1,15 @@
 import logging
 import whcfix.settings as settings
-from whcfix.data.yorkshirehockeyassociationadapter import YorkshireHockeyAssociationAdapter
-from whcfix.data.fixturesliveadapter import FixturesLiveAdapter
+import whcfix.data.yorkshirehockeyassociationadapter as yha
+import whcfix.data.fixturesliveadapter as fl
 from whcfix.data.picklecache import PickleCache
 
 
 class MatchesBase(object):
     '''MatchBase handles initiating the class and making the data available.'''
 
-    def __init__(self, auto_init_data=True):
+    def __init__(self, config, auto_init_data=True):
+        self.config = config
         self.listOfMatches = []
         try:
             if auto_init_data:
@@ -26,24 +27,18 @@ class MatchesBase(object):
             pc.dump(self.listOfMatches)
 
     def configGenerator(self):
-        for config in self.getConfig()["configs"]:
-            yield config
-
-    def getConfig(self):
-        return settings.CONFIGS
+        for c in self.config["configs"]:
+            yield c
 
     def getMatchesFromConfig(self, config):
         if config['dataSource']['source'] == 'YorkshireHA':
-            leagueId = config['dataSource']['league']
-            clubId = config['dataSource']['club']
+            leagueId = config['dataSource']['leagueId']
+            clubId = config['dataSource']['clubId']
             sectionName = config['sectionName']
-            adapter = YorkshireHockeyAssociationAdapter(leagueId,
-                                                        clubId, sectionName)
-            return adapter.get_matches()
+            return yha.get_matches(sectionName, leagueId)
         elif config['dataSource']['source'] == 'FixturesLive':
-            code = config['dataSource']['code']
-            name = config['dataSource']['name']
-            teamName = config['teamName']
             sectionName = config['sectionName']
-            adapter = FixturesLiveAdapter(code, name, teamName, sectionName)
-            return adapter.get_matches()
+            fixLiveNumber = config['dataSource']['fixLiveNumber']
+            club_name = config['dataSource']['club_name']
+            league = config['dataSource']['league']
+            return fl.get_matches(sectionName, fixLiveNumber, club_name, league)
