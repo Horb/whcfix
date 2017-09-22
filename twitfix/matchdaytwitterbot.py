fa55da42a28b5c866e24b3b71d0bc0957df556b5
Adam Morgan <horb.adam@gmail.com>
2017-09-23 00:36:20 +0100

    Fix timezone issues in matchdaytwitterbot.

--- twitfix/matchdaytwitterbot.py
+++ twitfix/matchdaytwitterbot.py
@@ -1,10 +1,30 @@
-from datetime import datetime
-
 import tweepy
+import random
+from datetime import datetime, timedelta
+from pytz import timezone
+london = timezone('Europe/London')
 
 from twitfix.credentials import *
 from whcfix.logic.matches import Matches
 
+
+def get_pitch_from_note(note):
+    if "Blue Pitch." in note:
+        return " on the Blue Pitch"
+    if "Green Pitch." in note:
+        return " on the Green Pitch"
+    return ""
+
+def get_opener():
+    return random.choice([
+        "Next Up",
+        "Coming Up",
+        "On Next",
+        "Starting Soon",
+        "Next Match",
+        "Warming Up",
+        "Next On"
+        ])
 
 def get_tweets(the_datetime, interval_minutes):
     interval_in_seconds = interval_minutes * 60
@@ -14,36 +34,46 @@ def get_tweets(the_datetime, interval_minutes):
             if current_match._time is None:
                 continue
 
+            if current_match.isPostponed:
+                continue
+
             full_date = datetime(
                                 current_match._date.year,
                                 current_match._date.month,
                                 current_match._date.day,
                                 current_match._time.hour,
                                 current_match._time.minute,
-                                current_match._time.second)
+                                current_match._time.second,
+                                tzinfo=london)
 
             td = full_date - the_datetime
 
             if 0 < td.total_seconds() <= interval_in_seconds:
-                if current_match.note == "":
-                    current_tweets.append("Next Up: %s vs. %s, %s push back #MatchDayBot"
-                        % (current_match.home, current_match.away, current_match.time))
-                else:
-                    current_tweets.append("Next Up: %s vs. %s on the %s %s push back #MatchDayBot"
-                        % (current_match.home, current_match.away, current_match.note.lower(), current_match.time))
+                pitch = get_pitch_from_note(current_match.note)
+                opener = get_opener()
+                template = "%s: %s vs %s%s, %s push back #GreenArmy"
+                tweet = template % (
+                        opener,
+                        current_match.home,
+                        current_match.away,
+                        pitch,
+                        current_match.time)
+                yield tweet
 
 
-def send_tweets(collected_tweets):
+def send_tweets(tweets):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     
-    for tweet in collected_tweets:
+    for tweet in tweets:
         if 0 < len(tweet) <= 140:
             api.update_status(status=tweet)
 
+def print_tweets(tweets):
+    for tweet in tweets:
+        print(tweet)
 
 if __name__ == '__main__':
-    current_tweets = []
-    get_tweets(datetime.now(), 30)
-    send_tweets(current_tweets)
+    d = london.localize(datetime.utcnow())
+    send_tweets(get_tweets(d, 30))
